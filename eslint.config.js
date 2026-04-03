bc6ae291e58364e85b4cabf7c87156100a51a887
kimyoo <kimyoo.ngov@gmail.com>
2026-04-03 23:02:08 +0700

    fix all commands errors and update dependency

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,6 @@ import ignores from './eslint/rules/ignores.js';
 import namingConventionRules from './eslint/rules/naming-convention.js';
 import sonarjsRules from './eslint/rules/sonarjs.js';
 import storybookRules from './eslint/rules/storybook.js';
-import svelteRules from './eslint/rules/svelte.js';
 import typescriptRules from './eslint/rules/typescript.js';
 import unicornRules from './eslint/rules/unicorn.js';
 
@@ -21,7 +20,6 @@ export default defineConfig(
 	...codeQualityRules,
 	...typescriptRules,
 	...namingConventionRules,
-	...svelteRules,
 	...unicornRules,
 	...sonarjsRules,
 	...storybookRules,
